a3ed4bec9400df40eb4b96445de2dfd893b40ffd
Markus Mannheim <markus.mannheim@gmail.com>
2020-03-30 14:25:10 +1100

    scheduled update

--- resources/scrape.js
+++ resources/scrape.js
@@ -17,30 +17,13 @@ async function scrape() {
         let cells = document.querySelectorAll("#table42313 td");
         return { actConfirmedCases: cells[0].innerText,
                  actNegativeTests: cells[1].innerText,
-                 actRecovered: cells[2].innerText
+                 actRecovered: cells[2].innerText,
+                 actDead: cells[3].innerText
                };
       });
       console.log(actData);
-      console.log("load NSW page");
-      await page.goto("https://www.health.nsw.gov.au/Infectious/diseases/Pages/covid-19-lga.aspx");
-      await page.waitForSelector(".moh-rteTable-6");
-      console.log("scrape NSW page");
-      let nswData = await page.evaluate(function() {
-        let names = document.querySelector(".moh-rteTable-6").querySelectorAll("tbody tr td:first-child");
-        let cases = document.querySelector(".moh-rteTable-6").querySelectorAll("tbody tr td:last-child");
-        data = {};
-        councils = ["Queanbeyan-Palerang Regional", "Goulburn Mulwaree", "Eurobodalla", "Yass Valley", "Snowy Monaro Regional"];
-        for (i = 0; i < names.length; i++) {
-          if (councils.includes(names[i].innerText)) {
-            data[names[i].innerText.replace(" Regional", "")] = cases[i].innerText;
-          }
-        }
-        return data;
-      });
-      console.log(nswData);
       browser.close();
-      data = Object.assign(actData, nswData);
-      return resolve(data);
+      return resolve(actData);
     } catch (error) {
       return reject(error);
     }
