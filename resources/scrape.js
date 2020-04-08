be0cbf3118aab77db8d9574e68fc89bf39db59da
Markus Mannheim <markus.mannheim@gmail.com>
2020-04-08 20:32:09 +1000

    scheduled update

--- resources/scrape.js
+++ resources/scrape.js
@@ -13,13 +13,25 @@ async function scrape() {
       console.log("scrape data ...");
       data = await page.evaluate(function() {
         let cells = document.querySelectorAll("#table42313 td");
-        return { cases: cells[0].innerText,
-                 negative: cells[1].innerText,
-                 recovered: cells[2].innerText,
-                 dead: cells[3].innerText
-               };
+        let ages = document.querySelectorAll("td[headers='table81686r1c1'");
+        let data = {
+          cases: cells[0].innerText,
+          negative: cells[1].innerText,
+          recovered: cells[2].innerText,
+          dead: cells[3].innerText,
+          age1: ages[6].innerText,
+          age2: ages[7].innerText,
+          age3: ages[8].innerText,
+          age4: ages[9].innerText,
+          age5: ages[10].innerText,
+          age6: ages[11].innerText,
+        };
+        return data;
       });
-      console.log("collected these data:\n\n" + data);
+      console.log("collected these data:\n");
+      for (key in data) {
+        console.log(key + ": " + data[key]);
+      }
       browser.close();
       return resolve(data);
     } catch (error) {
--- resources/updater.bat
+++ resources/updater.bat
@@ -1,6 +1,7 @@
 @ECHO off
 TITLE ACT COVID-19 data update
 ECHO Scraping ACT Health COVID-19 data ...
+ECHO
 start /b /wait node .\scrape.js
 git add ..\*.*
 git commit -m "scheduled update"
