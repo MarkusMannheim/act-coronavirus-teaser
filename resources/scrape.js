7a8d0cd3a1ad7a86a7d613abf2b0f619d4916a84
Markus Mannheim <markus.mannheim@gmail.com>
2020-04-09 09:34:26 +1000

    fix updaters

--- resources/scrape.js
+++ resources/scrape.js
@@ -2,27 +2,38 @@ puppeteer = require("puppeteer"),
 fs = require("fs");
 
 async function scrape() {
-  console.log("scrape()");
+  console.log("\nestablish scraper ...");
   browser = await puppeteer.launch({headless: true}),
   page = await browser.newPage();
   return new Promise(async function(resolve, reject) {
-    console.log("begin promise()");
     try {
-      console.log("load ACT page");
+      console.log("load ACT Health website ...");
       await page.goto("https://www.covid19.act.gov.au/updates/confirmed-case-information");
       await page.waitForSelector("#table42313");
-      console.log("scrape ACT page");
-      let actData = await page.evaluate(function() {
+      console.log("scrape data ...");
+      data = await page.evaluate(function() {
         let cells = document.querySelectorAll("#table42313 td");
-        return { actConfirmedCases: cells[0].innerText,
-                 actNegativeTests: cells[1].innerText,
-                 actRecovered: cells[2].innerText,
-                 actDead: cells[3].innerText
-               };
+        let ages = document.querySelectorAll("td[headers='table81686r1c1'");
+        let data = {
+          cases: cells[0].innerText,
+          negative: cells[1].innerText,
+          recovered: cells[2].innerText,
+          dead: cells[3].innerText,
+          age1: ages[6].innerText,
+          age2: ages[7].innerText,
+          age3: ages[8].innerText,
+          age4: ages[9].innerText,
+          age5: ages[10].innerText,
+          age6: ages[11].innerText,
+        };
+        return data;
       });
-      console.log(actData);
+      console.log("\ncollected these data:");
+      for (key in data) {
+        console.log(key + ": " + data[key]);
+      }
       browser.close();
-      return resolve(actData);
+      return resolve(data);
     } catch (error) {
       return reject(error);
     }
@@ -32,7 +43,7 @@ async function scrape() {
 scrape()
   .then(function(data) {
     fs.writeFile("./latestCount.json", JSON.stringify(data), function(error) {
-      console.log("latestCount.json written");
+      console.log("\nlatestCount.json written");
     });
   })
   .catch(console.error);
