c071fda82b6bd787486ae3b7b3161c6872ed4e96
Markus Mannheim <markus@pop-os.localdomain>
2020-04-09 11:01:34 +1000

    Stash aside

--- resources/updater.bat
+++ resources/updater.bat
@@ -1,4 +1,7 @@
-node .\scrape.js
+@ECHO off
+TITLE ACT COVID-19 data update
+ECHO Scraping ACT Health COVID-19 data ...
+start /b /wait node .\scrape.js
 git add ..\*.*
 git commit -m "scheduled update"
 git push
