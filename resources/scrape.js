7b257ab5858c2597f76b56a7d3bb4d1e0a7a4a5f
Markus Mannheim <markus@mannheim.media>
2020-04-15 12:46:52 +1000

    Add files via upload

--- resources/scrape.js
+++ resources/scrape.js
@@ -1,49 +1,59 @@
 puppeteer = require("puppeteer"),
-fs = require("fs");
-
+       fs = require("fs"),
+       d3 = require("d3");
 async function scrape() {
-  console.log("\nestablish scraper ...");
-  browser = await puppeteer.launch({headless: true}),
+  console.log("establish scraper ...");
+  browser = await puppeteer.launch(),
   page = await browser.newPage();
   return new Promise(async function(resolve, reject) {
     try {
       console.log("load ACT Health website ...");
       await page.goto("https://www.covid19.act.gov.au/updates/confirmed-case-information");
-      await page.waitForSelector("#table42313");
-      console.log("scrape data ...");
-      data = await page.evaluate(function() {
-        let cells = document.querySelectorAll("#table42313 td");
-        let ages = document.querySelectorAll("td[headers='table81686r1c1'");
-        let data = {
-          cases: cells[0].innerText,
-          negative: cells[1].innerText,
-          recovered: cells[2].innerText,
-          dead: cells[3].innerText,
-          age1: ages[6].innerText,
-          age2: ages[7].innerText,
-          age3: ages[8].innerText,
-          age4: ages[9].innerText,
-          age5: ages[10].innerText,
-          age6: ages[11].innerText,
-        };
+      await page.waitForSelector(".col-md-12");
+      console.log("load Power BI dashboard ...");
+      await page.click(".col-md-12 a");
+      dashUrl = await page.evaluate(function() {
+        return document.querySelector(".col-md-12 a").href;
+      });
+      await page.goto(dashUrl);
+      await page.waitForSelector(".card")
+      latest = await page.evaluate(function() {
+        let data = [];
+        document.querySelectorAll(".card")
+          .forEach(function(d) {
+            data.push(d.querySelector("tspan").innerHTML);
+          });
         return data;
       });
-      console.log("\ncollected these data:");
-      for (key in data) {
-        console.log(key + ": " + data[key]);
-      }
+      latest = {
+        recovered: latest[0],
+        confirmed: latest[1],
+        deaths: latest[2],
+        date: latest[3]
+      };
+      console.log("scraped latest update:");
+      console.log(latest);
       browser.close();
-      return resolve(data);
+      return resolve(latest);
     } catch (error) {
       return reject(error);
     }
   });
 }
-
 scrape()
-  .then(function(data) {
-    fs.writeFile("./latestCount.json", JSON.stringify(data), function(error) {
-      console.log("\nlatestCount.json written");
+  .then(function(latest) {
+    console.log("checking time-series data ...");
+    fs.readFile("./actData.csv", "utf8", function(error, oldData) {
+      oldData = d3.csvParse(oldData);
+      console.log("latest update: " + latest.date);
+      console.log("previous update: " + oldData[oldData.length - 1].date);
+      if (latest.date !== oldData[oldData.length - 1].date) {
+        console.log("updating time-series data ...");
+        oldData.push(latest);
+        fs.writeFile("./actData.csv", d3.csvFormat(oldData, ["date", "confirmed", "recovered", "deaths"]), function(error) {
+            console.log("./actData.csv written");
+          });
+      }
     });
   })
   .catch(console.error);
