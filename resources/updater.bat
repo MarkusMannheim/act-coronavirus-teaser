db3b17ef598b63a95190461b057fa2ddb3367f32
Markus Mannheim <markus.mannheim@gmail.com>
2021-02-26 15:35:41 +1100

    scheduled update

--- resources/updater.bat
+++ resources/updater.bat
@@ -1,7 +1,7 @@
 @ECHO off
 TITLE ACT COVID-19 data update
 ECHO Scraping ACT Health COVID-19 data ...
-start /w py ./scrape.py
+start /w /b py ./scrape.py
 git add ..\*.*
 git commit -m "scheduled update"
 git push
