7bcfb9f540870e6cbff27e10e7a0aecdac26af4e
markusmannheim <markus@mannheim.media>
2021-03-14 19:43:46 +1100

    update

--- resources/scrape.py
+++ resources/scrape.py
@@ -12,25 +12,25 @@ data["date"] = pd.to_datetime(data["date"], format="%Y-%m-%d")
 
 # use a headless browser (saves time)
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("start-maximized");
+# chrome_options.add_argument("--headless")
 
 # set up the browser
 driver = webdriver.Chrome(options=chrome_options)
 
-# navigate to data
+# navigate to data table
 driver.get("https://www.covid19.act.gov.au/")
-driver.maximize_window()
 data_card = WebDriverWait(driver, 10).until(
     EC.visibility_of_element_located((By.CLASS_NAME, "spf-article-card--tabular"))
 )
 
 # extract values
 data_table = data_card.find_element_by_class_name("spf-article-card--tabular-table")
-active = data_table.find_element_by_css_selector(".col-lg-4 table td:nth-child(2)").get_attribute("innerText").strip()[:-7]
-confirmed = data_table.find_element_by_css_selector(".col-lg-4 table td:last-child").get_attribute("innerText").strip()[:-6]
-recovered = data_table.find_element_by_css_selector(".col-lg-4:last-child table td:last-child").get_attribute("innerText").strip()
+active = data_card.find_element_by_css_selector("table td:nth-child(2)").get_attribute("innerText").replace(" Active", "")
+confirmed = data_card.find_element_by_css_selector("table td:last-child").get_attribute("innerText").replace(" Total", "")
+recovered = data_table.find_element_by_css_selector(".col-lg-6:nth-child(3) table td:last-child").get_attribute("innerText")
 deaths = "3"
-date = data_card.find_element_by_css_selector(".spf-article-card--tabular-subtitle p").get_attribute("innerText")[-10:]
+date = data_card.find_element_by_css_selector("p").get_attribute("innerText")[-10:]
 date = pd.to_datetime(date, format="%d/%m/%Y")
 
 # if data is new, add it to file
