7e7e43fc3fd9211c78fcf3aaa40011e04f665bd7
Markus Mannheim <markus@mannheim.media>
2021-10-19 20:04:31 +1100

    update

--- data/dataClean.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv(
-    "caseData.csv",
-    parse_dates=["date"],
-    index_col="date"
-)
--- data/update.py
+++ data/update.py
@@ -7,82 +7,101 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PyPDF2 import PdfFileReader
 
-# load dataset
-print("checking existing data ...")
-data = pd.read_csv("./data.csv", parse_dates=["date"], index_col="date")
-last_date = data.index[-1]
+def vaxScrape(date):
+    print(f"checking vaccination data for {date:%B %-d, %Y} ...")
+    url = f"https://www.health.gov.au/sites/default/files/documents/{date:%Y}/{date:%m}/covid-19-vaccine-rollout-update-{date:%-d-%B-%Y}.pdf".lower()
 
-# use a headless browser (saves time)
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--start-maximized")
-driver = webdriver.Chrome(options=chrome_options)
+    try:
+        request = requests.get(url)
+        file = io.BytesIO(request.content)
+        reader = PdfFileReader(file)
+        contents = reader.getPage(5).extractText().split("\n")
 
-# scrape ACT data
-print("scraping ACT website ...")
-driver.get(f"https://www.covid19.act.gov.au/updates/act-covid-19-statistics?randomKey={random.randint(1, 9000)+999}")
-date = WebDriverWait(driver, 5).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, ".spf-article-card--tabular-subtitle p"))
-)
-date = pd.to_datetime(date.get_attribute("innerText").split(", ")[1])
-stats = driver.find_elements_by_class_name("stat-card-number")
-new = float(stats[0].get_attribute("innerText"))
-active = float(stats[1].get_attribute("innerText"))
-
-driver.close()
-driver.quit()
-
-# fill new data
-print(f"latest data for {date:%B %-d, %Y}:")
-data.at[date, "new"] = new
-print(f"{new:,.0f} new cases")
-data.at[date, "total"] = data.at[date - pd.Timedelta(days=1), "total"] + new
-print(f"{data.at[date, 'total']:,.0f} total cases")
-data.at[date, "dead"] = data.at[date - pd.Timedelta(days=1), "dead"]
-print(f"{data.at[date, 'dead']:,.0f} dead")
-data.at[date, "recovered"] = data.at[date, "total"] - data.at[date, "dead"] - active
-print(f"{data.at[date, 'recovered']:,.0f} recovered")
-print(f"{active:,.0f} active cases")
-
-# scrape missing vax data
-vaxDate = date
-while True:
-    if (data.at[vaxDate, "first"] == 0) or (np.isnan(data.at[vaxDate, "first"])):
-        print("scraping vaccination reports ...")
-        print(f"checking {vaxDate:%B %-d, %Y} ...")
-        url = f"https://www.health.gov.au/sites/default/files/documents/{vaxDate:%Y}/{vaxDate:%m}/covid-19-vaccine-rollout-update-{vaxDate:%-d-%B-%Y}.pdf".lower()
-        try:
-            request = requests.get(url)
-            file = io.BytesIO(request.content)
-            reader = PdfFileReader(file)
-            contents = reader.getPage(5).extractText().split("\n")
+        for i in range(9):
             try:
-                first = float(contents[contents.index("dose 1") + 2].strip().replace(",", ""))
-                second = float(contents[contents.index("dose 2") + 2].strip().replace(",", ""))
-            except:
-                first = float(contents[contents.index("-dose 1") + 2].strip().replace(",", ""))
-                second = float(contents[contents.index("-dose 2") + 2].strip().replace(",", ""))
-            data.at[vaxDate, "first"] = first
-            data.at[vaxDate, "second"] = second
-            print("data found:")
-            print(f"first doses: {first:,.0f}")
-            print(f"second doses: {second:,.0f}")
-        except:
-            print("no data available")
-        vaxDate = vaxDate - pd.Timedelta(days=1)
-    else:
-        break
+                first_16 = float(contents[contents.index("dose 1") + i + 1].strip().replace(",", ""))
+                first_12 = float(contents[len(contents) - contents[::-1].index("dose 1") + i].strip().replace(",", ""))
+                second_16 = float(contents[contents.index("dose 2") + i + 1].strip().replace(",", ""))
+                second_12 = float(contents[len(contents) - contents[::-1].index("dose 2") + i].strip().replace(",", ""))
 
-# fill in calculated fields
-print("calculating remaining fields ...")
-data["half"] = (data["first"] - data["second"]) / 344014
-data["full"] = data["second"] / 344014
-for i, date in enumerate(data.index):
-    data.at[date, "average"] = data.iloc[max(i - 6, 0):i + 1]["new"].mean()
-    if (i < 8) or (data.iloc[i - 8:i - 3]["new"].mean() == 0):
-        data.at[date, "reff"] = None
+            except:
+                first_16 = float(contents[contents.index("-dose 1") + i + 1].strip().replace(",", ""))
+                first_12 = float(contents[len(contents) - contents[::-1].index("-dose 1") + i].strip().replace(",", ""))
+                second_16 = float(contents[contents.index("-dose 2") + i + 1].strip().replace(",", ""))
+                second_12 = float(contents[len(contents) - contents[::-1].index("-dose 2") + i].strip().replace(",", ""))
+
+            vaxData.iloc[i] = [first_16 + first_12, second_16 + second_12]
+
+        vaxData["population"] = [25704340, 431826, 8176368, 247023, 5206400, 1771703, 541965, 6648564, 2675797]
+        vaxData["first_percent"] = vaxData["first"] / vaxData["population"]
+        vaxData["second_percent"] = vaxData["second"] / vaxData["population"]
+        vaxData.sort_values("second_percent", ascending=True, inplace=True)
+        vaxData["date"] = date
+        vaxData.to_csv("vaxData.csv")
+        print(f"data found; vaxData.csv written")
+
+    except:
+        print(f"data unavailable for this date")
+        date = date - pd.Timedelta(days=1)
+        vaxScrape(date)
+
+def caseScrape(date):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    print(f"scraping ACT cases for {date:%B %-d, %Y} ...")
+    driver.get(f"https://www.covid19.act.gov.au/news-articles/act-covid-19-update-{date:%-d-%B-%Y}".lower())
+
+    try:
+        outcomes = WebDriverWait(driver, 5).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".ultra-condensed td p"))
+        )
+        outcomes = [outcome.get_attribute("innerText") for outcome in outcomes if outcome.get_attribute("innerText") != ""]
+        data.at[date, "new"] = float(outcomes[2])
+        data.at[date, "active"] = float(outcomes[4])
+        data.at[date, "hospitalised"] = float(outcomes[8])
+        data.at[date, "intensive care"] = float(outcomes[10])
+        data.at[date, "dead"] = data["dead"].sum() - float(outcomes[12])
+        print("data found and recorded")
+
+    except:
+        print("no data found")
+
+    if today > date:
+        lastDate = date + pd.Timedelta(days=1)
+        caseScrape(lastDate)
+
     else:
-        data.at[date, "reff"] = data.iloc[i - 4:i + 1]["new"].mean() / data.iloc[i - 8:i - 3]["new"].mean()
-data.to_csv("data.csv", index_label="date")
-print(f"latest Reff: {data.iat[-1, 9]:.2f}")
-print("date.csv written")
+        driver.close()
+        driver.quit()
+        for i, indice in enumerate(data.index):
+            data.at[indice, "total"] = data.at[indice, "new"] + data.iloc[0:i]["new"].sum()
+            data.at[indice, "recovered"] = data.at[indice, "total"] - data.at[indice, "active"] - data.iloc[0:i + 1]["dead"].sum()
+            data.at[indice, "average"] = data.iloc[max([i - 6, 0]):i + 1]["new"].sum() / 7
+        print("case data check complete; caseData.csv written")
+        data.to_csv("caseData.csv")
+
+# CASE DATA
+
+print("checking existing case data ...")
+data = pd.read_csv("./caseData.csv", parse_dates=["date"], index_col="date")
+lastDate = data.index[-1] + pd.Timedelta(days=1)
+
+today = pd.to_datetime(pd.Timestamp("now").date())
+if today < lastDate:
+    print("case data is up-to-date")
+else:
+    caseScrape(lastDate)
+
+# VACCINATION DATA
+
+vaxData = pd.DataFrame(
+    index=["AUS", "ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"],
+    columns=["first", "second"]
+)
+vaxData.index.name = "jurisdiction"
+
+date = pd.Timestamp("now").date()
+vaxScrape(date)
