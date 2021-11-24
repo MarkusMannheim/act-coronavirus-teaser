ede9d2300397bd1a54e5551aacf469892e3d9c7e
Markus Mannheim <markus.mannheim@gmail.com>
2021-11-25 10:09:32 +1100

    update

--- data/updateWin.bat
+++ data/updateWin.bat
@@ -1,9 +1,9 @@
 @echo off
+title "COVID-19 case and vaccination update"
 
 @echo Starting COVID-19 case and vaccination update ...
-@echo
-
-title "COVID-19 case and vaccination update"
 
 git fetch --all
 git reset --hard origin/gh-pages
+
+py updateWin.py
