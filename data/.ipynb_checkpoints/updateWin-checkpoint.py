42364b8459b6a8a2f84ea53a432eaca3ad68f75e
Markus Mannheim <markus@mannheim.media>
2022-01-11 16:25:23 +1100

    update

--- data/.ipynb_checkpoints/updateWin-checkpoint.py
+++ data/.ipynb_checkpoints/updateWin-checkpoint.py
@@ -2,101 +2,106 @@ import io, requests
 import pandas as pd, numpy as np
 from PyPDF2 import PdfFileReader
 
-def vaxScrape(date):
-    print()
-    print(f"checking vaccination data for {date:%B %#d, %Y} ...")
+def vax(date):
+    ''' collects vaccine data from the federal Health Department '''
+    
+    print(f"Checking vaccine data for {date:%A, %B %#d} ...")
     url = f"https://www.health.gov.au/sites/default/files/documents/{date:%Y}/{date:%m}/covid-19-vaccine-rollout-update-{date:%#d-%B-%Y}.pdf".lower()
 
     try:
         request = requests.get(url)
         file = io.BytesIO(request.content)
         reader = PdfFileReader(file)
-        contents = reader.getPage(5).extractText().split("\n")
+        contents = reader.getPage(6).extractText().split("\n")
+        
+        try:                
+            first_16 = float(contents[contents.index("dose 1") + 2].strip().replace(",", ""))
+            second_16 = float(contents[contents.index("dose 2") + 2].strip().replace(",", ""))        
+            first_12 = float(contents[len(contents) - contents[::-1].index("dose 1") + 1].strip().replace(",", ""))
+            second_12 = float(contents[len(contents) - contents[::-1].index("dose 2") + 1].strip().replace(",", ""))
 
-        for i in range(9):
-            try:
-                first_16 = float(contents[contents.index("dose 1") + i + 1].strip().replace(",", ""))
-                first_12 = float(contents[len(contents) - contents[::-1].index("dose 1") + i].strip().replace(",", ""))
-                second_16 = float(contents[contents.index("dose 2") + i + 1].strip().replace(",", ""))
-                second_12 = float(contents[len(contents) - contents[::-1].index("dose 2") + i].strip().replace(",", ""))
+        except:
+            first_16 = float(contents[contents.index("-dose 1") + 2].strip().replace(",", ""))
+            second_16 = float(contents[contents.index("-dose 2") + 2].strip().replace(",", ""))
+            first_12 = float(contents[len(contents) - contents[::-1].index("-dose 1") + 1].strip().replace(",", ""))
+            second_12 = float(contents[len(contents) - contents[::-1].index("-dose 2") + 1].strip().replace(",", ""))
 
-            except:
-                first_16 = float(contents[contents.index("-dose 1") + i + 1].strip().replace(",", ""))
-                first_12 = float(contents[len(contents) - contents[::-1].index("-dose 1") + i].strip().replace(",", ""))
-                second_16 = float(contents[contents.index("-dose 2") + i + 1].strip().replace(",", ""))
-                second_12 = float(contents[len(contents) - contents[::-1].index("-dose 2") + i].strip().replace(",", ""))
-
-            vaxData.iloc[i] = [first_16 + first_12, second_16 + second_12]
-
-        vaxData["population"] = [25739256, 432266, 8189266, 246338, 5221170, 1773243, 541479, 6649159, 2681633]
-        vaxData["first_percent"] = vaxData["first"] / vaxData["population"]
-        vaxData["second_percent"] = vaxData["second"] / vaxData["population"]
-        vaxData.sort_values("second_percent", ascending=True, inplace=True)
-        vaxData["date"] = date
-        vaxData.to_csv("vaxData.csv")
-        print(f"data found; vaxData.csv written")
+        first = first_16 + first_12
+        second = second_16 + second_12
+        
+        contents = reader.getPage(2).extractText().split("\n")        
+        boosters = float(contents[contents.index("Number of doses administered as part of the Commonwealth aged and disability care rollout") + 7].strip().replace(",", ""))
+                
+        vaxData.loc[0] = [first, second, boosters, date - pd.Timedelta(days=1)]
+        print("Data found; vaxdata.csv written to file.")
+        vaxData.to_csv("vaxData.csv", index=False)
 
     except:
-        print(f"data unavailable for this date")
+        print(f"Data unavailable for this date.")
         date = date - pd.Timedelta(days=1)
-        vaxScrape(date)
+        vax(date)
 
-def caseScrape(date):
-    print()
+def cases(date):
+    ''' prompts user for ACT case data '''
+    
+    print()    
     print(f"Input case data for {date:%A, %B %#d, %Y}:")
-    data.at[date, "new"] = float(input("New cases: "))
-    data.at[date, "active"] = float(input("Active cases: "))
-    data.at[date, "hospitalised"] = float(input("In hospital: "))
-    data.at[date, "intensive care"] = float(input("In intensive care: "))
-    data.at[date, "ventilated"] = float(input("On ventilation: "))
-    data.at[date, "dead"] = float(input("Deaths in past day: "))
-    data.at[date, "tests"] = float(input("Negative tests returned: "))
+    caseData.at[date, "dead"] = float(input("Deaths in past day: "))    
+    caseData.at[date, "new"] = float(input("New cases: "))
+    caseData.at[date, "active"] = float(input("Active cases: "))
+    caseData.at[date, "hospitalised"] = float(input("In hospital: "))
+    caseData.at[date, "intensive care"] = float(input("In intensive care: "))
+    caseData.at[date, "ventilated"] = float(input("On ventilation: "))    
+    caseData.at[date, "tests"] = float(input("Negative tests returned: "))
 
     if today > date:
-        date = date + pd.Timedelta(days=1)
-        if today == date:
-            todayCase(date)
-        else:
-            caseScrape(date)
-
-def todayCase(date):
-    print()
-    ready = input("Are today's case data available yet? (Y/N) ")
-    if ready.lower() == "y":
-        caseScrape(date)
+        checkToday(date + pd.Timedelta(days=1))
+    
     else:
-        print("Remember to check later today.")
+        clean()
+
+def checkToday(date):
+    ''' checks whether today's data is available '''
+
+    if today == date:
+        print()
+        ready = input("Are today's case data available yet? (Y/N) ")
+
+        if ready.lower() == "y":
+            cases(date)
+
+        else:
+            print("Remember to check later today.")
+
+    else:
+        cases(date)
 
 def clean():
     print()
-    print("cleaning irregular data ...")
-    for i, indice in enumerate(data.index):
-        data.at[indice, "total"] = data.at[indice, "new"] + data.iloc[0:i]["new"].sum()
-        data.at[indice, "recovered"] = data.at[indice, "total"] - data.at[indice, "active"] - data.iloc[0:i + 1]["dead"].sum()
-        data.at[indice, "average"] = data.iloc[max([i - 6, 0]):i + 1]["new"].sum() / 7
-    print("cleaning complete; caseData.csv written")
-    data.to_csv("caseData.csv")
+    print("Cleaning irregular data ...")
+    for i, date in enumerate(caseData.index):
+        caseData.at[date, "total"] = caseData.at[date, "new"] + caseData.iloc[0:i]["new"].sum()
+        caseData.at[date, "recovered"] = caseData.at[date, "total"] - caseData.at[date, "active"] - caseData.iloc[0:i + 1]["dead"].sum()
+        caseData.at[date, "average"] = caseData.iloc[max([i - 6, 0]):i + 1]["new"].sum() / 7
+        caseData.at[date, "positivity"] = caseData.at[date, "new"] / (caseData.at[date, "new"] + caseData.at[date, "tests"]) if pd.notna(caseData.at[date, "tests"]) else np.nan
+    
+    print("Cleaning complete; caseData.csv written to file.")
+    caseData.to_csv("caseData.csv")
 
-# CASE DATA
-print("checking existing case data ...")
-data = pd.read_csv("./caseData.csv", parse_dates=["date"], index_col="date")
-lastDate = data.index[-1] + pd.Timedelta(days=1)
+# prepare data        
+vaxData = pd.DataFrame(columns=["first", "second", "boosters", "date"])
+caseData = pd.read_csv("caseData.csv", parse_dates=["date"], index_col="date")
+lastDate = caseData.index[-1]
 today = pd.to_datetime(pd.Timestamp("now").date())
 
-if today < lastDate:
-    print("case data is up-to-date")
-elif today == lastDate:
-    todayCase(lastDate)
+# commence program
+print()
+vax(today)
+
+if today == lastDate:
+    print()
+    print("Case data are up-to-date.")
+
 else:
-    caseScrape(lastDate)
-clean()
-
-# VACCINATION DATA
-vaxData = pd.DataFrame(
-    index=["AUS", "ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"],
-    columns=["first", "second"]
-)
-vaxData.index.name = "jurisdiction"
-
-date = pd.Timestamp("now").date()
-vaxScrape(date)
+    date = lastDate + pd.Timedelta(days=1)
+    checkToday(date)
